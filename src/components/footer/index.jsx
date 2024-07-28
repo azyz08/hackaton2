e83280a669c72a796658e25366db3f062ad43dbd
Abduxabir2024 <abduqayumovabduxabir@gmail.com>
2024-07-28 14:53:27 +0500

    footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,8 +1,29 @@
+import { useTranslation } from "react-i18next";
 import "./style.scss"
 export default function Footer() {
+
+    const { t } = useTranslation();
     return (
         <>
-            <h1>Footer</h1>
+        <div className="Fotter">
+            <div className="bir">
+                <img src="./logo.png" alt="" />
+            </div>
+            <ul>
+                <p>{t('home')}</p>
+            </ul>
+            <ul>
+                <p>{t('universities')}</p>
+            </ul>
+            <ul>
+                <p>{t('studyCenters')}</p>
+            </ul>
+            <div className="ikki">
+            <i class="fa-brands fa-github"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-facebook"></i>
+            </div>
+        </div>
         </>
     )
 }
